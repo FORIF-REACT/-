0b1c3b642a15b45e88a3c8c70c4a0bc2e5613697
Lee junmin <terabyte4096@gmail.com>
2024-05-27 00:35:03 +0900

    Remove select label

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,7 +1,6 @@
 import { Fragment, useState } from "react";
 import {
   Listbox,
-  Label,
   ListboxButton,
   ListboxOptions,
   ListboxOption,
@@ -16,7 +15,6 @@ interface Item<T> {
 }
 
 interface Props<T> {
-  label: string;
   items: Item<T>[];
   onSelectItem: (selected: T) => void;
 }
@@ -38,9 +36,6 @@ export default function Select<T>(props: Props<T>) {
     <Listbox value={selected} onChange={selectItem}>
       {({ open }) => (
         <>
-          <Label className="block font-sans text-base font-medium leading-6 text-gray-900">
-            {props.label}
-          </Label>
           <div className="relative mt-2">
           <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-primary sm:text-sm sm:leading-6">
               <span className="flex items-center">
