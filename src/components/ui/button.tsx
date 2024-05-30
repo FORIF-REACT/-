a99f7c871077d51f790b0a6be6f7715d63a4b858
Lee junmin <terabyte4096@gmail.com>
2024-05-30 16:31:37 +0900

    Update button component to use variant parameter

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,29 +1,21 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
-
-import { cn } from "@/lib/utils"
+import { clsx } from "clsx";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap border-2 font-sans font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        default: "bg-white border-primary text-primary hover:border-primary/70",
+        filled: "bg-primary border-primary text-white hover:bg-primary/80",
+        slate: "bg-slate-400 border-slate-400 text-slate-[#44475D] hover:bg-slate-400/80",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        sm: "h-[30px] rounded-full px-4 text-12",
+        default: "h-[40px] rounded-md px-9 text-12",
+        lg: "w-full h-[50px] rounded-md text-20",
       },
     },
     defaultVariants: {
@@ -44,7 +36,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={clsx(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
@@ -53,4 +45,4 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 )
 Button.displayName = "Button"
 
-export { Button, buttonVariants }
+export default Button;
