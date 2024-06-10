e46a886b5180310aa48857a147149e68919307d0
purple-eye-orange <12sh20@naver.com>
2024-06-10 17:49:29 +0900

    기능 추가: 에러페이지,관리자페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,34 @@ import Register from "@/pages/Register";
 import RegisterDone from "@/pages/RegisterDone";
 import LectureRegister from "@/pages/LectureRegister";
 import Navbar from "./components/ui/navbar";
-import Profile from "@/pages/Profile";
-import MyProfile from "@/pages/MyProfile";
+import BasicModify from "./pages/Profile/BasicModify";
+import MentorModify from "./pages/Profile/MentorModify";
 import Main from "./pages/Main";
-import BasicModify from "./pages/BasicModify";
-import MentorModify from "./pages/MentorModify";
+import MyProfile from "./pages/Profile/MyProfile";
+import Profile from "./pages/Profile/Profile";
+import Error from "./pages/Error";
+import Admin from "./pages/Admin";
+import CheckModal from "./pages/CheckModal";
+
+
 function App() {
   return (
     // <div className="h-screen w-screen flex flex-col gap-4 justify-start items-center">
-    //     {/* <Navbar /> */}
-    //     <div className="max-w-[1100px] w-full mx-auto">
-    //       {/* <Login></Login> */}
-    //       {/* <Register /> */}
-    //       {/* <RegisterDone></RegisterDone> */}
-    //       {/* <LectureRegister></LectureRegister> */}
-    //       {/* <Profile /> */}
-    //     </div>
+    //   {/* <Navbar /> */}
+    //   <div className="max-w-[1100px] w-full mx-auto">
+    //     {/* <Login></Login> */}
+    //     {/* <Register /> */}
+    //     {/* <RegisterDone></RegisterDone> */}
+    //     {/* <LectureRegister></LectureRegister> */}
+    //     {/* <Profile /> */}
+    //   </div>
     // </div>
-      <Main />
-      // <BasicModify />
-      // <MyProfile />
-      // <MentorModify />
+    // <Main />
+    // <MyProfile />
+    // <Error />
+    <Admin />
+    // <CheckModal />
+
   );
 }
 
---
+++ src/pages/CheckModal.tsx
@@ -0,0 +1,15 @@
+import Footer from '@/components/ui/Footer'
+import Navbar from '@/components/ui/navbar'
+import React from 'react'
+
+const CheckModal = () => {
+  return (
+    <>
+      <Navbar />
+      
+      <Footer />
+    </>
+  )
+}
+
+export default CheckModal
