2958130741a01d6a5e36311d9dc117b6da846de3
purple-eye-orange <12sh20@naver.com>
2024-06-22 18:06:30 +0900

    api 생성

--- src/apis/common.ts
+++ src/apis/common.ts
@@ -2,6 +2,15 @@ import axios from "axios";
 
 const context = "http://localhost:3000";
 
+export const post = async <T extends Record<string, any>> (path: string, params: T) => {
+  return await axios.post(context + path, params);
+}
 export const get = async <T, U>(path: string, params: T) => {
   return await axios.get(context + path, { params }).then((response) => response.data) as U;
+}
+export const put = async <T extends Record<string, any>> (path: string, params: T ) => {
+  return await axios.put(context + path, params);
+}
+export const Delete = async (path: string) => {
+  return await axios.delete(context + path);
 }
